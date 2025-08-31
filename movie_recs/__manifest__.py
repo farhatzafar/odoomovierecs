9e781943d95af2e9623884133118d8ef8ddd2473
Farhat Zafar <farhatzafar63@gmail.com>
2025-08-31 14:12:16 -0400

    add menu and action to display list of all movies, add access rights

--- movie_recs/__manifest__.py
+++ movie_recs/__manifest__.py
@@ -4,6 +4,6 @@
     'author': 'Farhat',
     'description': "A module that displays a catalog of movies. And recommends movies based on user preference.",
     'depends': ['base'],
-    'applicatiom': True,
-    
+    'application': True,
+    'data': ['security/ir.model.access.csv', 'views/movie_views.xml', 'views/movie_menus.xml']
 }
