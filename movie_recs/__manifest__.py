b36d7b35ac591812ec286cdd86383116593a467d
Farhat Zafar <farhatzafar63@gmail.com>
2025-08-31 12:38:35 -0400

    add manifest and init files for module

---
+++ movie_recs/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Movie Recommendations",
+    'version': '18.0.0.1',
+    'author': 'Farhat',
+    'description': "A module that displays a catalog of movies. And recommends movies based on user preference."
+    'depends': ['base'],
+    'applicatiom': True,
+    
+}
