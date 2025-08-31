1f9580fc8430bc447da31e4dd5823d89a65bba80
Farhat Zafar <farhatzafar63@gmail.com>
2025-08-31 13:08:18 -0400

    add movie model and import in init files

--- movie_recs/__manifest__.py
+++ movie_recs/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Movie Recommendations",
     'version': '18.0.0.1',
     'author': 'Farhat',
-    'description': "A module that displays a catalog of movies. And recommends movies based on user preference."
+    'description': "A module that displays a catalog of movies. And recommends movies based on user preference.",
     'depends': ['base'],
     'applicatiom': True,
     
---
+++ movie_recs/models/movie.py
@@ -0,0 +1,38 @@
+from odoo import fields, models
+
+class Movie(models.model):
+
+    _name = "movie_recs.movie"
+    _description= "Stores info about movies"
+
+    title = fields.Char(string="Title", required=True)
+
+    genre = fields.Selection(
+        string="Genre",
+        selection=[
+            ('horror', 'Horror'),
+            ('drama', 'Drama'),
+            ('comedy', 'Comedy')
+            ]
+    )
+
+    release_year = fields.Selection(
+        string="Release Year",
+        selection=[
+            ('2020', '2020'),
+            ('2021', '2021'),
+            ('2022', '2022'),
+            ('2023', '2023'),
+            ('2024', '2024'),
+            ]
+    )
+
+    lead_actor_gender = fields.Selection(
+        string="Lead Actor Gender",
+        selection=[
+            ('male', 'Male'),
+            ('female', 'Female')
+            ]
+    )
+
+    poster_url = fields.Char(string="Poster URL")
